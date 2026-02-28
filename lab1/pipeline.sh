2256a546b9442623b12b002a72baba21092ae90a
Константин Реент <mikril10426@gmail.com>
2026-02-28 14:57:05 +0500

    second commit

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+# Установка pip, если нет
+if ! command -v pip &> /dev/null; then
+    sudo apt update && sudo apt install -y python3-pip
+fi
+
 required_libs=("pandas" "numpy" "scikit-learn")
 
 for lib_name in "${required_libs[@]}"; do
     if ! python3 -c "import $lib_name" &> /dev/null; then
-        pip3 install $lib_name
+        pip install --break-system-packages "$lib_name"
     fi
 done
 
